fdf041c36e2aa580feb2e8d8f0f29ae92205ece3
yalitzab <yalitzabueno@gmail.com>
2020-10-04 13:34:00 -0700

    responsive: github username

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Professional-README-Generator
+# asd
+
+## Description 
+asd
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+
+asd
+
+## Usage 
+
+fds
+
+
+## Credits
+
+yalitzab
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+---
+  
+
+## Badges
+
+[![GitHub last commit](https://img.shields.io/github/last-commit/google/skia.svg?style=flat)]()
+
+## Contributing
+
+[GitHub: yalitzab](https://github.com/yalitzab)
+
+## Tests
+
+asd
+
+## Questions
+
+[GitHub profile link: yalitzab](https://github.com/yalitzab)
+
+You can email me at asdvf if you have any questions!
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,62 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description 
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage 
+
+${data.usage}
+
+
+## Credits
+
+${data.contributing}
+
+## License
+
+${data.license === "Apache"? "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)": 
+  data.license === "MIT"? "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)": 
+  data.license === "Mozilla"? "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)(http://unlicense.org/)"}
+
+---
+  
+
+## Badges
+
+${data.badge === 
+  "commit"? "[![GitHub last commit](https://img.shields.io/github/last-commit/google/skia.svg?style=flat)]()": 
+  data.badge === 
+  "issue"? "[![Issues](https://img.shields.io/github/issues-raw/tterb/PlayMusic.svg?maxAge=25000)](https://github.com/tterb/Hyde/issues)": 
+  data.badge === 
+  "pull"? "[![GitHub pull requests](https://img.shields.io/github/issues-pr/cdnjs/cdnjs.svg?style=flat)]()": 
+  "[![NPM Version](https://img.shields.io/npm/v/npm.svg?style=flat)]()"}
+
+## Contributing
+
+[GitHub: ${data.contributing}](https://github.com/${data.contributing})
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+[GitHub profile link: ${data.username}](https://github.com/${data.username})
+
+You can email me at ${data.email} if you have any questions!
 `;
 }
 
